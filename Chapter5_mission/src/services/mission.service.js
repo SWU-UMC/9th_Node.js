2b70d4523345ba53bfa0c43a982c31d546f97f2b
ywkim1m <ywkim1m@gmail.com>
2025-11-05 20:10:30 +0900

    Chore: 불필요한 폴더 Git 추적에서 제외 (.github, 5주차 폴더, database)

--- Chapter5_mission/src/services/mission.service.js
+++
@@ -1,8 +0,0 @@
-// src/services/mission.service.js
-import { addMission } from "../repositories/mission.repository.js";
-import { responseFromMission } from "../dtos/mission.dto.js";
-
-export const createMission = async (missionData) => {
-  const mission = await addMission(missionData);
-  return responseFromMission(mission);
-};
