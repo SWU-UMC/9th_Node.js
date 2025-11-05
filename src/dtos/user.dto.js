5dd5b2f25fbc2fb55a6246b13cd9a4923f75e2ed
Chea-yunzi <katete051@naver.com>
2025-11-05 14:12:45 +0900

    ✨   실습  ORM 사용해보기  완료

--- src/dtos/user.dto.js
+++ src/dtos/user.dto.js
@@ -1,3 +1,36 @@
+// 요청 본문 → User 객체로 변환
+export const bodyToUser = (body) => {
+  const birth = new Date(body.birth); 
+
+  return {
+    email: body.email, 
+    name: body.name, 
+    gender: body.gender, 
+    birth, 
+    address: body.address || "", 
+    detailAddress: body.detailAddress || "", 
+    phoneNumber: body.phoneNumber, 
+    preferences: body.preferences, 
+  };
+};
+
+// 워크북 응답 dto
+export const responseFromUser = ({ user, preferences }) => {
+  const preferFoods = preferences.map(
+    (preference) => preference.foodCategory.name
+  );
+
+  return {
+    email: user.email,
+    name: user.name,
+    preferCategory: preferFoods,
+  };
+};
+
+
+
+
+
 /*export const bodyToUser = (body) => {
     const birth = new Date(body.birth); //날짜 변환
   
