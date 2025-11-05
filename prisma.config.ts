1adfdf10a0d1aa599514fc8c54d0bbe933560506
Chea-yunzi <katete051@naver.com>
2025-11-05 14:38:04 +0900

    🎉  DB 마이그레이션 파일 관리 실습 완료

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
   datasource: {
     url: env("DATABASE_URL"),
   },
+  output: {
+    client: "./node_modules/.prisma/client", // 기본 경로로 설정
+  },
 });
 
 //환경 변수 불러올 수 있도록 수정.
