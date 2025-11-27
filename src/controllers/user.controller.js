099c206a25dc4dbf19e1795639cf0c6504d4959f
suyeon0421 <157108684+suyeon0421@users.noreply.github.com>
2025-11-27 17:04:43 +0900

    미션 09 jwt, google 로그인

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 import { bodyToUser } from "../dtos/user.dto.js";
-import { userSignUp } from "../services/user.service.js";
+import { userSignUp, userUpdateInfo } from "../services/user.service.js";
 
 export const handleUserSignUp = async (req, res, next) => {
   /*
@@ -73,12 +73,27 @@ export const handleUserSignUp = async (req, res, next) => {
   console.log("회원가입을 요청했습니다!");
 
   console.log("body:", req.body); // 값이 잘 들어오나 확인하기 위한 테스트용
+
 	
   try {
     const user = await userSignUp(bodyToUser(req.body));
     
-    res.status(StatusCodes.CREATED(201)).success(user);
+    res.status(StatusCodes.CREATED).success(user);
   } catch (error) {
     next(error);
   }
+};
+
+export const handleUserUpdateInfo = async (req, res, next) => {
+  const userId = req.user.id; 
+  
+  const updateData = req.body; 
+
+  try {
+    const updatedUser = await userUpdateInfo(userId, updateData); 
+
+    res.status(StatusCodes.OK).success(updatedUser);
+  } catch (error) {
+    next(error);
+  }
 };
