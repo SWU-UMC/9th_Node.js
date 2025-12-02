f371f6a231c32f5b9699d584b002d83a22f944cb
Chea-yunzi <katete051@naver.com>
2025-12-02 17:29:09 +0900

    서버 외부 접근 access

--- src/index.js
+++ src/index.js
@@ -126,4 +126,7 @@ app.use((err, req, res, next) => {
    서버 실행
 ============================== */
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
